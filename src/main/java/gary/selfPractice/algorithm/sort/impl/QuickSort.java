36ad5b9beeb5abce8fdcde2bc06d389274f13199
Gary <gary258796@gmail.com>
2021-06-13 20:54:49 +0800

    feat: Add Quick Sort and test of it

--- src/main/java/gary/selfPractice/algorithm/sort/impl/QuickSort.java
+++ src/main/java/gary/selfPractice/algorithm/sort/impl/QuickSort.java
@@ -1,4 +1,69 @@
 /* Licensed of Gary Liao (C)2021 */
 package gary.selfPractice.algorithm.sort.impl;
 
-public class QuickSort {}
+import gary.selfPractice.algorithm.sort.Sort;
+import java.util.List;
+import java.util.Objects;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuickSort extends Sort<Integer> {
+
+  /**
+   * Algorithm(Divide and Conquer)
+   *
+   * <p>quickSort(arr[], low, high) { if(low<high){ // partition is responsible to place pivotIndex
+   * in correct place of array pivotIndex = partition(arr, low, high); // divide and conquer
+   * quickSort(arr, low, pivotIndex-1); quickSort(arr, pivotIndex+1, high); } }
+   *
+   * <p>// pivot element will be evert last element partition(array, low, high) { // Store the index
+   * of elements that are smaller then array[right] int i = low-1; for(j=low; j < high; j++ ) { //
+   * compare all element with array[right] if(array[j] <= array[right]) { // if j element is smaller
+   * or equals to our pivot element // swap j to the front part where we put all elements that are
+   * smaller than pivot i++; swap(array[i], array[j]); } } // after finishing arrange other
+   * elements, put our pivot into correct place swap(i+1, high); return i+1; }
+   */
+  @Override
+  public List<Integer> sorting(List<Integer> sortedData) {
+    quickSort(sortedData, 0, sortedData.size() - 1);
+    return sortedData;
+  }
+
+  /**
+   * Main part of quickSort
+   *
+   * @param unSortedData:
+   * @param low:
+   * @param high:
+   */
+  private void quickSort(List<Integer> unSortedData, int low, int high) {
+    if (low < high) {
+      int pivotIndex = partition(unSortedData, low, high);
+      quickSort(unSortedData, low, pivotIndex - 1);
+      quickSort(unSortedData, pivotIndex + 1, high);
+    }
+  }
+
+  /**
+   * Sort the subList, by putting pivot to correct place and others in bigger or smaller place
+   *
+   * @param unSortedData: Data unSorted totally
+   * @param low: the min index of list
+   * @param high: the max index of list
+   * @return A List with pivot index in correct place in List and bigger and smaller elements in
+   *     right position.
+   */
+  private Integer partition(List<Integer> unSortedData, Integer low, Integer high) {
+    // The biggest index of elements that are smaller then array[right]
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (isBigger(unSortedData.get(high), unSortedData.get(j))
+          || Objects.equals(unSortedData.get(high), unSortedData.get(j))) {
+        i++;
+        Swap(unSortedData, i, j);
+      }
+    }
+    Swap(unSortedData, i + 1, high);
+    return i + 1;
+  }
+}
---
+++ src/test/java/gary/selfPractice/algorithm/sort/impl/QuickSortTest.java
@@ -0,0 +1,108 @@
+/* Licensed of Gary Liao (C)2021 */
+package gary.selfPractice.algorithm.sort.impl;
+
+import gary.selfPractice.algorithm.recordTime.TimingExtension;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ExtendWith(TimingExtension.class)
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@DisplayName("Testing QuickSort...")
+@ComponentScan(basePackages = "gary.selfPractice.algorithm.sort.impl")
+public class QuickSortTest {
+
+  @Autowired private QuickSort quickSort;
+
+  @Tag("QuickSort")
+  @Order(1)
+  @DisplayName("Test empty list...")
+  @ParameterizedTest
+  @MethodSource("gary.selfPractice.algorithm.provider.TestDataProvider#emptyData")
+  void testEmptySort(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    Assertions.assertEquals("[]", sortedData.toString());
+  }
+
+  @Tag("QuickSort")
+  @Order(2)
+  @DisplayName("Test size 1 list...")
+  @ParameterizedTest
+  @MethodSource("gary.selfPractice.algorithm.provider.TestDataProvider#oneElementData")
+  void testSizeOneSort(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    Assertions.assertEquals("[10]", sortedData.toString());
+  }
+
+  @Tag("QuickSort")
+  @Order(3)
+  @DisplayName("Test size 3 list...")
+  @ParameterizedTest
+  @MethodSource("gary.selfPractice.algorithm.provider.TestDataProvider#threeElementData")
+  void testSizeThreeSort(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    System.out.println(sortedData.toString());
+    Assertions.assertEquals("[1, 2, 3]", sortedData.toString());
+  }
+
+  @Tag("QuickSort")
+  @Order(4)
+  @DisplayName("Test list include null at first element...")
+  @ParameterizedTest
+  @MethodSource(
+      "gary.selfPractice.algorithm.provider.TestDataProvider#includeNullElementDataAtFirst")
+  void testDataWithNullAtFirst(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    System.out.println(sortedData.toString());
+    Assertions.assertEquals("[1, 2, 3, null]", sortedData.toString());
+  }
+
+  @Tag("QuickSort")
+  @Order(5)
+  @DisplayName("Test list include null at middle element...")
+  @ParameterizedTest
+  @MethodSource(
+      "gary.selfPractice.algorithm.provider.TestDataProvider#includeNullElementDataAtMiddle")
+  void testDataWithNullAtMiddle(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    System.out.println(sortedData.toString());
+    Assertions.assertEquals("[1, 2, 3, null]", sortedData.toString());
+  }
+
+  @Tag("QuickSort")
+  @Order(6)
+  @DisplayName("Test list include multiple null elements...")
+  @ParameterizedTest
+  @MethodSource("gary.selfPractice.algorithm.provider.TestDataProvider#includeNullElementsData")
+  void testDataWithNullElements(List<Integer> testData) {
+    List<Integer> sortedData = quickSort.sorting(testData);
+    System.out.println(sortedData.toString());
+    Assertions.assertEquals("[1, 2, 3, 10, null, null, null]", sortedData.toString());
+  }
+
+  @Test
+  @Tag("QuickSort")
+  @Order(7)
+  @DisplayName("Test list with random 1000 data...")
+  void testDataWithRandomData() {
+    final int size = 1000;
+    List<Integer> orderedList = new ArrayList<>();
+    List<Integer> unSortedData = new ArrayList<>();
+    Random randomNum = new Random(1);
+    for (int i = 0; i < size; i++) orderedList.add(i);
+    for (int i = size; i > 0; i--) unSortedData.add(orderedList.remove(randomNum.nextInt(i)));
+    for (int i = 0; i < size; i++) orderedList.add(i);
+
+    List<Integer> sortedData = quickSort.sorting(unSortedData);
+    System.out.println(sortedData.toString());
+    Assertions.assertEquals(sortedData.toString(), orderedList.toString());
+  }
+}
